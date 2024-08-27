155a1929c563a4c023e7b964e1c24a3d1337edaa
xcvi-dev <xcvi.dev@gmail.com>
2024-08-27 20:37:16 +0200

    - Updated BroadcastReciever

--- app/src/main/java/com/xcvi/stepcounter/service/RebootBroadcastReceiver.kt
+++ app/src/main/java/com/xcvi/stepcounter/service/RebootBroadcastReceiver.kt
@@ -9,8 +9,15 @@ import javax.inject.Inject
 
 class RebootBroadcastReceiver: BroadcastReceiver() {
 
-    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     override fun onReceive(context: Context?, intent: Intent?) {
+        when (intent?.action){
+            Intent.ACTION_REBOOT -> restartService(context, intent)
+            Intent.ACTION_BOOT_COMPLETED -> restartService(context, intent)
+            Intent.ACTION_LOCKED_BOOT_COMPLETED -> restartService(context, intent)
+        }
+    }
+
+    private fun restartService(context: Context?, intent: Intent?){
         Intent(context, SensorService::class.java).also {
             it.action = SensorService.Actions.RESTART.name
             context?.startForegroundService(it)
