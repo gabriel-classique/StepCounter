6b6ff5b125b31321b060a45420eb09495780d4d9
xcvi-dev <xcvi.dev@gmail.com>
2024-08-29 13:13:31 +0200

    - accelerometer

--- app/src/main/java/com/xcvi/stepcounter/service/RebootBroadcastReceiver.kt
+++ app/src/main/java/com/xcvi/stepcounter/service/RebootBroadcastReceiver.kt
@@ -1,23 +1,20 @@
 package com.xcvi.stepcounter.service
 
-import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.xcvi.stepcounter.data.StepsRepository
-import javax.inject.Inject
 
 class RebootBroadcastReceiver: BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         when (intent?.action){
-            Intent.ACTION_REBOOT -> restartService(context, intent)
-            Intent.ACTION_BOOT_COMPLETED -> restartService(context, intent)
-            Intent.ACTION_LOCKED_BOOT_COMPLETED -> restartService(context, intent)
+            Intent.ACTION_REBOOT -> restartService(context)
+            Intent.ACTION_BOOT_COMPLETED -> restartService(context)
+            Intent.ACTION_LOCKED_BOOT_COMPLETED -> restartService(context)
         }
     }
 
-    private fun restartService(context: Context?, intent: Intent?){
+    private fun restartService(context: Context?){
         Intent(context, SensorService::class.java).also {
             it.action = SensorService.Actions.RESTART.name
             context?.startForegroundService(it)
